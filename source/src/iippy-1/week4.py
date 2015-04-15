257cb040e439028f2cc7a479f5a149c161fbbf32
chenjieGu <xytxjt@gmail.com>
2015-04-15 14:15:18 +0800

    add dir week5

--- source/src/iippy-1/week4.py
+++ source/src/iippy-1/week4.py
@@ -3,8 +3,13 @@ import math
 import random
 
 #init 
+record_list = []
+botoom
 
-def range_choose
+#helper functions
+
+def range_choose:
+
 def number_choose
 
 def guess_AI
@@ -12,4 +17,12 @@ def guess_AI
 def compare_number
 
 def timer
+
+#event handlers
+
+#create frame
+
+#register event handlers
+
+#start game
 
